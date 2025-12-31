406731976fa52f4ec0ded036958259898a5af274
Oz <hypertexterx@pm.me>
2025-12-30 16:50:09 -0800

    fixes nvim treesitter startup error due to migration

--- dotfiles/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/plugins/treesitter.lua
@@ -2,27 +2,24 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter.configs").setup({
-      -- A list of parser names, or "all"
+    -- Use pcall so that if it fails, it doesn't break your whole Neovim
+    local status, configs = pcall(require, "nvim-treesitter.configs")
+    if not status then
+      return
+    end
+
+    configs.setup({
       ensure_installed = {
         "json", "javascript", "typescript", "tsx", "yaml", "html", "css",
-        "c", "cpp", "go",
-        "csv", "diff", "xml", "mermaid",
-        "markdown", "markdown_inline", "bash",
-        "lua", "vim", "dockerfile", "gitignore",
+        "c", "cpp", "go", "lua", "vim", "vimdoc",
+        "csv", "diff", "xml", "markdown", "markdown_inline", "bash",
       },
-
-      -- Install parsers synchronously (only applied to `ensure_installed`)
       sync_install = false,
-
-      -- Automatically install missing parsers when entering a buffer
       auto_install = true,
-
       highlight = {
         enable = true,
-        additional_vim_regex_highlighting = {"markdown"},
+        additional_vim_regex_highlighting = { "markdown" },
       },
-
       indent = { enable = true },
     })
   end,
